1ffd21f701d814d44f07a4b5a1621c7785f15b26
Shameema Ashraf <229136227+Shameema007@users.noreply.github.com>
2026-02-28 10:07:07 +0530

    Add files via upload

---
+++ Blood Link/script.js
@@ -0,0 +1,26 @@
+// // Login Form Redirect
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     const loginForm = document.getElementById("loginForm");
+
+//     if (loginForm) {
+//         loginForm.addEventListener("submit", function(e) {
+//             e.preventDefault();
+
+//             // For now just redirect
+//             window.location.href = "role.html";
+//         });
+//     }
+
+// });
+
+// // Role Buttons
+// function goToDonor() {
+//     // alert("Redirecting to Donor Form (create donor.html next)");
+//     window.location.href="donar.html";
+// }
+
+// function goToReceiver() {
+//     // alert("Redirecting to Receiver Form (create receiver.html next)");
+//     window.location.href="receiver.html";
+// }
